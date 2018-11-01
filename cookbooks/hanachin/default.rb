886d896bfe9d9672c2d449eff27b91cb2907450e
Seiei Miyagi <hanachin@gmail.com>
2018-11-01 17:35:04 +0900

    Add hanachin cookbooks

---
+++ cookbooks/hanachin/default.rb
@@ -0,0 +1,16 @@
+# rcm
+execute "wget -qO /etc/yum.repos.d/utilities.repo https://download.opensuse.org/repositories/utilities/RHEL_7/utilities.repo" do
+  not_if "[ -f /etc/yum.repos.d/utilities.repo ]"
+end
+
+package "rcm"
+
+remote_directory "/home/ec2-user/.dotfiles" do
+  owner "ec2-user"
+  group "ec2-user"
+  source "dotfiles"
+end
+
+execute "rcup" do
+  user "ec2-user"
+end
